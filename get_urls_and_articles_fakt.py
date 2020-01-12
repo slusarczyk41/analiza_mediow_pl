987400ce31e03dc52d957fb9eebb721cd4b662b4
slusarczyk41 <slusarczyk41@gmail.com>
2020-01-12 17:21:33 +0100

    notebook with data preprocessing and title/short heatmap first steps

--- get_urls_and_articles_fakt.py
+++ get_urls_and_articles_fakt.py
@@ -7,10 +7,10 @@ from time import sleep
 
 
 
-# o = Options()
-# o.add_argument('--headless')
-# driver = Chrome(options = o)
-driver = Chrome()
+o = Options()
+o.add_argument('--headless')
+driver = Chrome(options = o)
+# driver = Chrome()
 
 
 done_keywords = listdir('data/fakt/urls')
@@ -60,33 +60,46 @@ for keyword in keywords:
     fakt_content = []
     
     for article_url in article_urls:
-        driver.get(article_url)
-        if 'onet.pl' in article_url:
-            if len(driver.find_elements_by_xpath('//*[@id="mainPageBody0"]/section/div/article/main/div/div/div/a')) == 1:
+        try:
+            driver.get(article_url)
+            if 'onet.pl' in article_url:
+                if len(driver.find_elements_by_xpath('//*[@id="mainPageBody0"]/section/div/article/main/div/div/div/a')) == 1:
+                    try:
+                        if 'onet.pl' in driver.current_url:
+                            driver.find_element_by_xpath('//*[@id="mainPageBody0"]/section/div/article/main/div/div/div/a').click()
+                            title = driver.find_element_by_class_name('detailTitle').text.strip()
+                            short = driver.find_element_by_class_name('leadDetail').text.strip()
+                            long = " ".join([x.text for x in driver.find_elements_by_class_name('hyphenate') if len(x.find_elements_by_tag_name('a')) == 0])
+                            desc = "-@@@-".join([x.text for x in driver.find_elements_by_class_name('imgDesc')])
+                            comments = None
+                            onet_content.append([article_url, title, short, long, desc, comments])
+                        else:
+                            title = driver.find_element_by_class_name('title').text.strip()
+                            short = driver.find_element_by_class_name('detailTitle').text.strip()
+                            article_body = driver.find_element_by_class_name('articleBody')
+                            long = " ".join([x.text for x in article_body.find_elements_by_class_name('hyphenate') if len(x.text.split(' ')) > 10])
+                            desc = "-@@@-".join([x.text for x in driver.find_elements_by_class_name('imgDesc')])
+                            comments = None
+                            fakt_content.append([article_url, title, short, long, desc, comments])
+                    except:
+                        error_urls.append(article_url)
+                else:
+                    not_handled_urls.append(article_url)
+            elif 'fakt.pl' in article_url:
                 try:
-                    driver.find_element_by_xpath('//*[@id="mainPageBody0"]/section/div/article/main/div/div/div/a').click()
-                    title = driver.find_element_by_class_name('detailTitle').text.strip()
-                    short = driver.find_element_by_class_name('hyphenate').text.strip()
-                    long = " ".join([x.text for x in driver.find_elements_by_class_name('hyphenate') if len(x.find_elements_by_tag_name('a')) == 0])
+                    title = driver.find_element_by_class_name('title').text.strip()
+                    short = driver.find_element_by_class_name('detailTitle').text.strip()
+                    article_body = driver.find_element_by_class_name('articleBody')
+                    long = " ".join([x.text for x in article_body.find_elements_by_class_name('hyphenate') if len(x.text.split(' ')) > 10])
                     desc = "-@@@-".join([x.text for x in driver.find_elements_by_class_name('imgDesc')])
                     comments = None
-                    onet_content.append([article_url, title, short, long, desc, comments])
+                    fakt_content.append([article_url, title, short, long, desc, comments])
                 except:
                     error_urls.append(article_url)
             else:
                 not_handled_urls.append(article_url)
-        elif 'fakt.pl' in article_url:
-            try:
-                title = driver.find_element_by_class_name('title').text.strip()
-                short = driver.find_element_by_class_name('hyphenate').text.strip()
-                long = " ".join([x.text for x in driver.find_elements_by_class_name('hyphenate') if len(x.find_elements_by_tag_name('a')) == 0])
-                desc = "-@@@-".join([x.text for x in driver.find_elements_by_class_name('imgDesc')])
-                comments = None
-                fakt_content.append([article_url, title, short, long, desc, comments])
-            except:
-                error_urls.append(article_url)
-        else:
-            not_handled_urls.append(article_url)
+        except:
+            error_urls.append(article_url)
             
     with open('data/fakt/articles/'+keyword, 'a') as f:
         writer =  csv.writer(f)
